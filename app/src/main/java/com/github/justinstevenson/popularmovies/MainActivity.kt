cc32d79d47ef51ca905312cea214047ea05999ea
Justin Stevenson <justinrstevenson@yahoo.com>
2022-04-18 21:34:51 -0400

    Add entry point to MainActivity

--- app/src/main/java/com/github/justinstevenson/popularmovies/MainActivity.kt
+++ app/src/main/java/com/github/justinstevenson/popularmovies/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.justinstevenson.popularmovies.ui.theme.PopularMoviesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
